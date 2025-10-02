c1103b2abbfb8870651b80d2551aad64b2509c8e
Shwe Hlaing <shwe@pixelsquad.com.sg>
2025-10-02 13:52:33 +1300

    Removed .env and OllamaConfig Fixes

--- src/Modules/Configs/OllamaConfig.js
+++ src/Modules/Configs/OllamaConfig.js
@@ -1,8 +1,6 @@
 
 // export const OLLAMA_API_URL = process.env.OLLAMA_API_URL
 // export const OLLAMA_MODEL = process.env.OLLAMA_MODEL
-// export const CHAT_WEB_SERVICES_URL = process.env.CHAT_WEB_SERVICES_URL
 
-export const OLLAMA_API_URL="http://127.0.0.1:11434/api/generate"
-export const OLLAMA_MODEL="llama3"
-export const CHAT_WEB_SERVICES_URL="http://127.0.0.1:8000/ask"
+export const OLLAMA_API_URL="https://sociologistically-supermarine-karon.ngrok-free.dev/api/generate"
+export const OLLAMA_MODEL="gemma3:4b"
